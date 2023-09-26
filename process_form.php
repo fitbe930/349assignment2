03375cf52e19dbf0c237676766d662935d5c04ad
Ben Fitzpatrick <b_fitzpatrick@hotmail.co.nz>
2023-09-27 02:28:50 +1300

    connection to database and database query working

---
+++ process_form.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+
+<head>
+    <meta charset="UTF-8">
+
+
+</head>
+
+<?php
+$dbhost = 'db.cgmjlbiqninb.us-east-1.rds.amazonaws.com';
+$dbuser = 'user';
+$dbpass = 'password';
+$dbname = 'studs';
+
+// Create a connection to the database
+$conn = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
+
+// Check the connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} else{
+    echo "Connected to database";
+}
+
+
+$firstname = $_POST['firstname'];
+$lastname = $_POST['lastname'];
+$course = $_POST['course'];
+$stud_id = $_POST['stud_id'];
+
+$sql = "INSERT INTO Students (firstname, lastname, course, stud_id)
+        VALUES ('$firstname', '$lastname', '$course', '$stud_id')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Record added successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+      
+}
+$conn -> close();
+?>
+<!DOCTYPE html>
+<body>
+    <a href ="index.html">
+        <button>Return</button>
+</body>
