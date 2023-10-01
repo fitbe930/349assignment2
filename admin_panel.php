7d2d3fb533a88222ac09b2825c9b1ee5f287067c
Ben Fitzpatrick <b_fitzpatrick@hotmail.co.nz>
2023-10-01 13:43:49 +1300

    tidy up

--- admin_panel.php
+++ admin_panel.php
@@ -8,8 +8,6 @@ $conn = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
-} else{
-    echo "Connected to database";
 }
 
 // Fetch student records from the database
--- process_form.php
+++ process_form.php
@@ -17,8 +17,6 @@ $conn = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
-} else{
-    echo "Connected to database";
 }
 
 
